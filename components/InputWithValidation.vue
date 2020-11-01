0028f7f96550ab7118c6273a806e2077f71acc43
HwanChang <ghksckd219@gmail.com>
2020-11-02 02:05:37 +0900

    feat: input 검증 component 추가

---
+++ components/InputWithValidation.vue
@@ -0,0 +1,36 @@
+<template>
+  <ValidationProvider v-slot="{ errors, valid }" :rules="rules">
+    <v-text-field
+      v-model="innerValue"
+      :label="label"
+      :error-messages="errors"
+      :success="valid"
+      v-bind="$attrs"
+      v-on="$listeners"
+    />
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from 'vee-validate'
+export default {
+  components: {
+    ValidationProvider
+  },
+  props: {
+    rules: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      innerValue: ''
+    }
+  }
+}
+</script>
